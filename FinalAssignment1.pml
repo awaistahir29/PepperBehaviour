bca761f10496c76d2137fac3af0aa280cee3999f
Awais Tahir <awaistahir29@gmail.com>
2022-12-17 16:55:42 +0100

    Added Behaviour to interact with Pepper Robot

---
+++ FinalAssignment1.pml
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="FinalAssignment1" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="Start" src="Start/Start.dlg" />
+        <Dialog name="move" src="move/move.dlg" />
+        <Dialog name="PoseAsking" src="PoseAsking/PoseAsking.dlg" />
+        <Dialog name="Startt" src="Startt/Startt.dlg" />
+        <Dialog name="Intro" src="Intro/Intro.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+        <Topic name="Start_enu" src="Start/Start_enu.top" topicName="Start" language="en_US" />
+        <Topic name="move_enu" src="move/move_enu.top" topicName="move" language="en_US" />
+        <Topic name="PoseAsking_enu" src="PoseAsking/PoseAsking_enu.top" topicName="PoseAsking" language="en_US" />
+        <Topic name="Startt_enu" src="Startt/Startt_enu.top" topicName="Start" language="en_US" />
+        <Topic name="Intro_enu" src="Intro/Intro_enu.top" topicName="Intro" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Which pose do you like?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Which pose do you like?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you ready? Cheeeeez</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you ready? Cheeeeez</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>i am taking picture from the front</source>
+            <comment>Text</comment>
+            <translation type="unfinished">i am taking picture from the front</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Are you ready? Cheeeeez</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Are you ready? Cheeeeez</translation>
+        </message>
+        <message>
+            <source>Cheezzeeee</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Cheezzeeee</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I am taking picture from front, Chezeeee</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am taking picture from front, Chezeeee</translation>
+        </message>
+        <message>
+            <source>Cheezee</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Cheezee</translation>
+        </message>
+        <message>
+            <source>smile</source>
+            <comment>Text</comment>
+            <translation type="obsolete">smile</translation>
+        </message>
+        <message>
+            <source>ready</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ready</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Cheezeee</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Cheezeee</translation>
+        </message>
+        <message>
+            <source>cheezeee !</source>
+            <comment>Text</comment>
+            <translation type="obsolete">cheezeee !</translation>
+        </message>
+        <message>
+            <source>I am taking picture from the front.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am taking picture from the front.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am taking picture from the front</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am taking picture from the front</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Taking Picture/Animated Say</name>
+        <message>
+            <source>How do you want me to take the picture of you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">How do you want me to take the picture of you?</translation>
+        </message>
+        <message>
+            <source>Which pose do you like?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Which pose do you like?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Taking Picture/Animated Say (2)</name>
+        <message>
+            <source>Are you ready? Cheeeeez</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Are you ready? Cheeeeez</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Talking/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Let me capture a picture of you</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Let me capture a picture of you</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Talking/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Which pose do you like?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Which pose do you like?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Talking/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Talking/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+</TS>
